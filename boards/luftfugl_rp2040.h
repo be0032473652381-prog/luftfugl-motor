3d157b1ac849305616ee8dabbec510a2368a6cb9
Henrik Andersen <be0032473652381@gmail.com>
2026-08-29 10:26:01 +0200

    Set confirmed RP2040 flash size to 4MB

--- boards/luftfugl_rp2040.h
+++ boards/luftfugl_rp2040.h
@@ -5,7 +5,7 @@
 #define PICO_DEFAULT_UART_TX_PIN      0
 #define PICO_DEFAULT_UART_RX_PIN      1
 #define PICO_FLASH_SPI_CLKDIV         2
-#define PICO_FLASH_SIZE_BYTES         (16 * 1024 * 1024)
+#define PICO_FLASH_SIZE_BYTES         (4 * 1024 * 1024)   // confirmed via SFDP, see agent.md §1
 #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
 #define PICO_RP2040_B0_SUPPORTED      0
 
